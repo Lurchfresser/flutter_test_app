7591536484304b537e181ba48690d6bd136c8955
Lurchfresser <Tritrop007@gmx.de>
2023-11-19 23:24:36 +0100

    initial commit

---
+++ lib/navigation.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test_app/presentation/pages/home_screen/home_screen.dart';
+import 'package:flutter_test_app/presentation/pages/test_animation_screen/test_animation.dart';
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  final List<(String, IconData)> _navigationChildren = [
+    ("Home", Icons.home),
+    ("Test Animation", Icons.science),
+  ];
+  final List<Widget> _bodyChildren = [
+    const HomeScreen(title: "Home Screen"),
+    const TestAnimation(),
+  ];
+
+  int _currentIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          for (var item in _navigationChildren)
+            BottomNavigationBarItem(
+              icon: Icon(item.$2),
+              label: item.$1,
+            )
+        ],
+        onTap: (index) => _onItemTapped(index),
+        currentIndex: _currentIndex,
+      ),
+      body: Center(
+        child: _bodyChildren[_currentIndex],
+      ),
+    );
+  }
+}
---
+++ lib/presentation/pages/test_animation_screen/test_animation.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TestAnimation extends StatelessWidget {
+  const TestAnimation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
